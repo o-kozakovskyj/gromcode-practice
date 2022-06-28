4899314ec961b4bfbd0171090937b026f0b370e6
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-06-28 19:47:07 +0300

    calculator

--- JS/lesson-13/home2/index.js
+++
@@ -1,9 +0,0 @@
-const countOccurrences = (text = '', subtext) => {
-  if (subtext.length === 0) {
-    return null;
-  }
-  const startLength = text.length;
-  const endLength = text.split(subtext).join('').length;
-  return (startLength - endLength) / subtext.length;
-};
-console.log(countOccurrences( ,''));
