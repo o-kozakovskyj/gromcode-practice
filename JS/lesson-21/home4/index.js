e9bf72454ebd52cfc126aa5c517a236cf30f5c19
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-07 21:21:46 +0300

    hz

--- JS/lesson-21/home4/index.js
+++ JS/lesson-21/home4/index.js
@@ -1,7 +1,7 @@
 export function setTitle(text) {
 const titleElem = document.querySelector('.title');
 titleElem.textContent = text;
-return titleElem.textContent
+
 }
 console.dir(setTitle('text'));
 
