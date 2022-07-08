ea978fa60233b4ea2fb7af6f36a75621d26d8dfd
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-08 10:29:54 +0300

    home9

---
+++ JS/lesson-21/home9/index.js
@@ -0,0 +1,10 @@
+export function finishForm() {
+  const form = document.querySelector('.login-form');
+  const input  = document.createElement('input');
+  input.name = "login"
+  form.prepend(input)
+const secondInput = document.querySelector('[name="password"]');
+secondInput.type = "password"
+}
+
+// finishForm()
--- JS/lesson-21/home9/reverse.js
+++
@@ -1,8 +0,0 @@
-export const reverseArray = (numbers) => {
-  if (!Array.isArray(numbers)) {
-    return null;
-  }
-  return [...numbers].reverse();
-};
-console.log(reverseArray([1, 4, 3, 6]));
-console.log(reverseArray('567'));
