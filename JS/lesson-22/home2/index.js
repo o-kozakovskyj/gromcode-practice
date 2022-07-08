f37f036beb1bbda8aa94dccf5f6f32b2c9fc64a8
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-08 21:07:07 +0300

    change

--- JS/lesson-22/home2/index.js
+++ JS/lesson-22/home2/index.js
@@ -1,14 +1,5 @@
-export function getItemsList() {
-  const elementsList = document.querySelectorAll('.technology');
-  console.dir(elementsList);
-  return elementsList;
+const inputElem = document.querySelector('.text-input');
+function changer() {
+  console.log(inputElem.value)
 }
-export function getItemsArray() {
-  const NodeList = document.querySelectorAll('.tool');
-  const elementsArray = Array.from(NodeList)
-  console.dir(elementsArray)
-  return elementsArray;
-}
-
-getItemsList()
-getItemsArray()
+inputElem.addEventListener('change', changer)
