7abea6b7e5f48892cb70cb1bd67a00a04ae4321a
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-08 17:18:07 +0300

    task1

--- JS/lesson-21/task1/index.js
+++ JS/lesson-21/task1/index.js
@@ -1,8 +1,37 @@
-function getEvenNumbers(arr) {
-  return arr.filter((number) => number % 2 === 0);
-}
-it('shoud get only even nums', () => {
-  // eslint-disable-next-line no-undef
-  const result = getEvenNumbers([1, 2, 3, 4, 5, 6]);
-  expect(result).toEqual([2, 4, 6]);
-});
+'use strict';
+
+export const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
+
+/**
+ * @param {object[]} tasksList
+ * @return {undefined}
+ */
+const renderTasks = tasksList => {
+  const listEl = document.querySelector('.list');
+
+  const listItemsElements = tasksList.sort((a, b) => a.done - b.done)
+  .map(({ text, done}) => {
+    const listItemEl = document.createElement('li');
+    listItemEl.classList.add('list__item');
+    if (done) {
+      listItemEl.classList.add('list__item_done')
+      console.log( listItemEl)
+   }
+    const checkEl = document.createElement('input');
+    checkEl.classList.add('.list__item-checkbox');
+    checkEl.setAttribute('type','checkbox');
+    checkEl.checked = done;
+    listItemEl.append(checkEl,text);
+    return listItemEl;
+  });
+listEl.append(...listItemsElements)
+};
+
+
+renderTasks(tasks)
