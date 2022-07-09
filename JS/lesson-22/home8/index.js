464aada300e85962aed42a6d33f30bd682dc9a15
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-09 11:03:09 +0300

    home8

--- JS/lesson-22/home8/index.js
+++ JS/lesson-22/home8/index.js
@@ -1,18 +1,6 @@
-export function finishList() {
-  const liOne = document.createElement('li');
-  const liEight = document.createElement('li');
-  const liSix = document.createElement('li');
-  const liFour = document.createElement('li');
-  const ul = document.querySelector('ul')
-  const liFive = document.querySelector('.special')
-  liEight.textContent = '8'
-  liOne.textContent = '1';
-  liFour.textContent = '4';
-  liSix.textContent = '6';
-  ul.prepend(liOne);
-  ul.append(liEight);
-  liFive.before(liFour);
-  liFive.after(liSix);
+const checkElem = document.querySelector('.task-status');
+function handleCheck(event) {
+  console.log(checkElem.checked);
+  console.log(event.target.checked);
 }
-
-// finishList()
+checkElem.addEventListener('change', handleCheck);
