21701d70f7cfb8210ec1a976474104540daacd50
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-09 10:54:34 +0300

    home7

--- JS/lesson-22/home7/index.js
+++ JS/lesson-22/home7/index.js
@@ -1,5 +1,6 @@
 const inputEl = document.querySelector('.text-input');
-function handleChange() {
+function handleChange(event) {
   console.log(event.target.value)
 }
+
 inputEl.addEventListener('change', handleChange)
