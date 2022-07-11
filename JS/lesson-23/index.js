30d7d5ba9cd48bf8e2fb8e98d208db86940a952d
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-11 16:51:20 +0300

    form fixed

--- JS/lesson-23/index.js
+++ JS/lesson-23/index.js
@@ -1,30 +1,30 @@
-const passwordEl = document.querySelector('#password1');
-const emailEl = document.querySelector('#email1');
+const passwordEl = document.querySelector('#password');
+const emailEl = document.querySelector('#email');
 const errPassword = document.querySelector('.error-text_password');
 const errEmail = document.querySelector('.error-text_email');
-console.log(errPassword)
+
 const isRequired = value => {
-  value
-    ? undefined
-    : 'Required';
+  if (value.length > 0) {
+    return undefined
+  }
+  return 'Required';
 }
-const isEmail= value => {
-  value.includes('@')
-    ? undefined
-    : 'Should be an email'
- }
+const isEmail = value => {
+  if (value.includes('@') === true) {
+    return undefined
+  }
+  return 'Should be an email'
+}
 const onEmailChange = event => {
   const errorText = [isRequired, isEmail]
     .map(validator => validator(event.target.value))
-    .filter(errorText => errorText)
-    .join(', ');
+    .filter(value => value)
+    .join(', ')
   console.log(errorText)
-    errEmail.textContent = errorText;
+  errEmail.textContent = errorText;
+
 }
-
 emailEl.addEventListener('input', onEmailChange)
-
-
 
 const onPasswordChange = event => {
   const errorText = [isRequired]
@@ -34,12 +34,12 @@ const onPasswordChange = event => {
     console.log(errPassword)
     errPassword.textContent = errorText;
 }
-passwordEl.addEventListener('input', onPasswordChange)
+passwordEl.addEventListener('input', onPasswordChange);
 
 const formElem = document.querySelector('.login-form');
 const onFormSubmit = event => {
-  event.preventDefault()
-  formData = Object.fromEntries(new FormData(formElem))
-  alert(formData);
+  event.preventDefault();
+  const formData = Object.fromEntries(new FormData(formElem))
+  alert(JSON.stringify(formData));
 }
-formElem.addEventListener(onFormSubmit('submit', onFormSubmit))
+formElem.addEventListener('submit', onFormSubmit)
