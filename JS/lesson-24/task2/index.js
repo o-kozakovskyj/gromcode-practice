a9ed74e5e1533cdb4406f878162791556fc60db6
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-13 17:11:16 +0300

    fix

--- JS/lesson-24/task2/index.js
+++ JS/lesson-24/task2/index.js
@@ -1,8 +1,8 @@
-export const formatter = new Intl.DateTimeFormat('en', {
+ const formatter = new Intl.DateTimeFormat('en', {
   timeZone: 'UTC',
   hour: '2-digit',
   minute: '2-digit',
   hour12: false
 })
-const getGreenwichTime = date => formatter.format(date)
+export const getGreenwichTime = date => formatter.format(date)
 console.log(getGreenwichTime(new Date))
