e85433326feb01cbcb3a787f4bc5b44eaf4c541e
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-13 16:10:59 +0300

    task1

--- JS/lesson-24/task1/index.js
+++ JS/lesson-24/task1/index.js
@@ -1,27 +1,10 @@
- const vehicle = {
-  name: 'Argo',
-  move() {
-    console.log(`${this.name} is moving`)
-  },
-  stop() {
-    console.log(`${this.name} stopped`)
-  }
+const weekDay = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St']
+export function dayOfWeek(date, days) {
+  const day = new Date(date).getDate();
+  const dateInFuture = new Date(date).setDate(day + days);
+  return weekDay[new Date(dateInFuture).getDay()];
 }
-
- const ship = {
-  startMachine() {
-    console.log(`${this.name} lifting anchor up`)
-    ship.move.bind(ship);
-  },
-  stopMachine() {
-   ship.stop();
-    console.log(`${this.name} lifting anchor down`)
-   
-  },
-  __proto__ : vehicle
-} 
 //---test data---
-console.log(ship)
-vehicle.move()
-ship.startMachine()
-ship.stopMachine()
+console.log(dayOfWeek(new Date(2020, 13, 7), 6))
+console.log(dayOfWeek(new Date(2022, 0, 1), 16))
+console.log(dayOfWeek(new Date(2022, 13, 7), 0))
