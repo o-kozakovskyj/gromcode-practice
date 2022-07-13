08a269f2a999fe557179360bce898bd4b30d69cb
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-13 19:52:56 +0300

    blank

--- JS/lesson-24/home1/index.js
+++ JS/lesson-24/home1/index.js
@@ -2,14 +2,14 @@
 
 
 export const getDiff = (startDate, endDate) => {
- const msDifference = Math.abs(new Date(endDate).getTime()-new Date(startDate).getTime()) 
-  const seconds = parseInt((msDifference/1000)%60);
-  const minutes = parseInt((msDifference/(1000*60))%60);
-  const hours = parseInt((msDifference/(1000*60*60))%24);
-  const days = parseInt(msDifference/(1000*60*60*24));
-  return (`${days}d ${hours}h  ${minutes}m ${seconds}s`);
+  const msDifference = Math.abs(new Date(endDate).getTime() - new Date(startDate).getTime())
+  const seconds = parseInt((msDifference / 1000) % 60);
+  const minutes = parseInt((msDifference / (1000 * 60)) % 60);
+  const hours = parseInt((msDifference / (1000 * 60 * 60)) % 24);
+  const days = parseInt(msDifference / (1000 * 60 * 60 * 24));
+  return (`${days}d ${hours}h ${minutes}m ${seconds}s`);
 }
 
-console.log(getDiff(new Date(2022, 1, 12), new Date(2022, 11, 12)));
+console.log(getDiff(new Date(2022, 1, 12), new Date(2022, 1, 11)));
 console.log(getDiff(new Date(2020, 1), new Date(2022, 11, 12, 16, 40, 45)));
 console.log(getDiff(new Date(2024, 10, 12), new Date(2020, 11, 12)));
