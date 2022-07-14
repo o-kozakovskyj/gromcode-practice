5b613a82a5a55f8f3a266b2cb5bbf7efec68e44d
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-14 08:33:13 +0300

    add24

--- JS/lesson-24/add1/index.js
+++ JS/lesson-24/add1/index.js
@@ -1,17 +1,9 @@
-const user = {
-  firstName: 'John',
-  lastName: 'Doe',
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`
-  },
-  set fullName(fullName) {
-    this.firstName = fullName.split(' ')[0];
-    this.lastName = fullName.split(' ')[1];
-  },
+export const studentsBirthDays = (students) => {
+  const month = ['Jan', 'Feb','Mar', 'April','May', 'Jun', 'Jul','Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+  console.log(month[new Date(students[0].birthDate).getMonth()]);
+  return [...students]
+    .map(student => student.month[new Date(student.birthDate).getMonth()] = new Date(student.birthDate).getDate());
 }
 
-//---test data----
-console.log(user.fullName);
-user.fullName = 'Bob Marley'
-console.log(user.fullName);
-console.log(user.firstName)
+const listOfStudents = [{ name: 'Tom', birthDate: '01/15/2010' }, { name: 'Bob', birthDate: '01/18/2010' }, { name: 'Bill', birthDate: '02/15/2010' }, { name: 'Jane', birthDate: '03/15/2010' }];
+console.log(studentsBirthDays(listOfStudents))
