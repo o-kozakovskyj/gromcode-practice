e9ec57cfe2d6e7f37b13fc8e28bbd64105990a3c
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-14 15:58:59 +0300

    ww

--- JS/lesson-9/additional3/index.js
+++ JS/lesson-9/additional3/index.js
@@ -2,26 +2,29 @@
 // get keys 1 get keys2 & compare
 // iterate key1 key2 value1 value2
 // if falls => return false
+
 function compareObjects(obj1, obj2) {
-  // if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-  //   return false;
-  // }
-  // return (
-  //   Object.keys(obj1).join('') === Object.keys(obj2).join('') &&
-  //   Object.values(obj1).join('') === Object.values(obj2).join('')
-  // );
-  return obj1.filter()
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+  return Object.keys(obj1).every(key => obj1[key] === obj2[key])
 }
 
-// examples
+//---test data---
 const obj1 = {
-  name: 'Tom',
+  name: 'Bob',
+  sex: 'male',
+  hobby: 'footbal',
   age: 17,
+  adress: 'Ukraine'
 };
 
 const obj2 = {
   name: 'Bob',
   age: 17,
+  sex: 'male',
+  hobby: 'footbal',
+  country: 'Ukraine'
 };
 
 const obj3 = {
@@ -31,8 +34,12 @@ const obj3 = {
 };
 
 const obj4 = {
-  name: 'Tom',
+  
+  sex: 'male',
+  hobby: 'footbal',
   age: 17,
+  adress: 'Ukraine',
+  name: 'Bob',
 };
 
 console.log(compareObjects(obj1, obj2)); // ==> false
