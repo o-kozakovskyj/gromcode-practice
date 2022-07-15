28d5fc325273e8d97fe98a770fa130676e7697e4
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-15 18:31:04 +0300

    chain

---
+++ JS/lesson-28/task1/index.js
@@ -0,0 +1,29 @@
+const calc = initValue => {
+  let result = initValue;
+ const calculator = {
+    add(number) {
+      result += number;
+      return this;
+    },
+    subtruct(number) {
+      result -= number;
+      return this;
+    },
+    mult(number) {
+      result *= number;
+      return this;
+    },
+    div(number) {
+      result /= number;
+      return this;
+    },
+    result() {
+      return result;
+   }
+  }
+  return calculator
+}
+// ------test data------
+
+console.log(calc(3).add(2).mult(4).div(10).subtruct(5).result())
+console.log(calc(3).result())
