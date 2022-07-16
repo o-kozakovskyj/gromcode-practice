9e421f68bffeeccd40e52f516678362bc78b6bcd
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-16 13:27:10 +0300

    date

---
+++ JS/lesson-28/home1/index.js
@@ -0,0 +1,45 @@
+//algo
+//get operation
+// get type of time
+// convert to ms
+// convert old Data to ms
+// make operation
+// return new date using ms result
+
+// export const shmoment = (initDate) => {
+
+const convertToMillisec = (type, number) => {
+  switch (type) {
+    case 'years': return number * 31536000000;
+    case 'months': return number * 2592000000;
+    case 'days': return number * 86400000
+    case 'hours': return number * 3600000;
+    case 'minutes': return number * 60000;
+    case 'seconds': return number * 1000;
+    case 'milliseconds': return number;
+  }
+}
+const convertData = (data) => new Data(data).getTime;
+
+const shmoment = initDate => {
+  let resultData = new Date(initDate).getTime();
+  console.log(resultData)
+  const hronometr = {
+    add(type, num) {
+      
+      resultData += convertToMillisec(type, num);
+      
+      return this;
+    },
+    subtract(type, num) {
+      resultData -= convertToMillisec(type, num);
+      return this;
+    },
+    result() {
+      return new Date(resultData);
+    }
+  }
+  return hronometr;
+}
+//--- test data---
+console.log(shmoment(new Date(2022, 7)).add('minutes', 2).add('years', 4).result())
