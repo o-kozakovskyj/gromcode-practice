2567ad9c95f935838bb81c4fb001147cb874a7b3
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-18 13:49:18 +0300

    ff

--- JS/lesson-29/task1/index.js
+++ JS/lesson-29/task1/index.js
@@ -26,5 +26,5 @@ const onImageLoaded = (error, data) => {
 };
 
 // examples
-const imgEl = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'
-addImage(imgEl,onImageLoaded);
+// const imgEl = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'
+// addImage(imgEl,onImageLoaded);
