97f7f24309d72929cf8ac59282003e1bda8a9b06
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-18 21:33:13 +0300

    test

---
+++ JS/lesson-29/add1/index.js
@@ -0,0 +1,15 @@
+const user = {
+  name: "Bob",
+  job: 'driver',
+  city: 'Prague',
+  sayHi(client) {
+    console.log(`Hi, i am your passanger ${client} ${this.name} - ${this.job} from ${this.city}`)
+  }
+}
+
+
+function delay(delay, callback, context, ...argument) {
+  setTimeout((()=>callback.call(context, argument)), delay)
+}
+
+delay(1000, user.sayHi,user,'Vasya', 'Dodic', 'Biba')
