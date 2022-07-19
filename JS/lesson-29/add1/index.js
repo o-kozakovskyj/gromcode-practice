356921b4020ce88294db85dad4470146dd4e2ef9
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-19 08:42:07 +0300

    ws

--- JS/lesson-29/add1/index.js
+++ JS/lesson-29/add1/index.js
@@ -17,8 +17,8 @@ const user1 = {
 
 
 export function delay(delay, callback, context,...args) {
-  setTimeout((callback.bind(context, args)), delay)
+  setTimeout((callback.call(context, args)), delay)
 }
 
-delay(10000, user.sayHi, user, 'Tom', 'John', 'Bob',1, 2,3)
-delay(1000, user.sayHi,user1,'Tom', 'John', 'Bob')
+// delay(10000, user.sayHi, user, 'Tom', 'John', 'Bob',1, 2,3)
+// delay(1000, user.sayHi,user1,'Tom', 'John', 'Bob')
