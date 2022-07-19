c5523bec3385f1a7a20643a998ec226369601969
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-19 08:37:43 +0300

    inp

--- JS/lesson-29/add1/index.js
+++ JS/lesson-29/add1/index.js
@@ -3,7 +3,7 @@ const user = {
   job: 'driver',
   city: 'Prague',
   sayHi(client) {
-    console.log(`Hi, i am your passanger ${client} ${this.name} - ${this.job} from ${this.city}`)
+    console.log(`Hi,-- ${client}--, I am ${this.name} - ${this.job} from ${this.city}`)
   }
 }
 const user1 = {
@@ -11,14 +11,14 @@ const user1 = {
   job: 'dealer',
   city: 'Paris',
   sayHi(client) {
-    console.log(`Hi, i am your passanger ${client} ${this.name} - ${this.job} from ${this.city}`)
+    console.log(`Hi, -- ${client}---, I am${this.name} - ${this.job} from ${this.city}`)
   }
 }
 
 
 export function delay(delay, callback, context,...args) {
-  setTimeout(()=>(callback.call(context,[].slice.call(args))), delay)
+  setTimeout((callback.bind(context, args)), delay)
 }
 
-delay(1000, user.sayHi, user, 'Vasya', 'Dodic', 'Biba',1, 2,3)
-delay(1000, user.sayHi,user1,'Vasya', 'Dodic', 'Biba')
+delay(10000, user.sayHi, user, 'Tom', 'John', 'Bob',1, 2,3)
+delay(1000, user.sayHi,user1,'Tom', 'John', 'Bob')
