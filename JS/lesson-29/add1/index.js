5584ea556acaef6c446610c2f3c62f7870d13bd8
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-19 10:50:07 +0300

    fix

--- JS/lesson-29/add1/index.js
+++ JS/lesson-29/add1/index.js
@@ -10,14 +10,14 @@ const user1 = {
   name: "Tom",
   job: 'dealer',
   city: 'Paris',
-  sayHi(client) {
-    console.log(`Hi, -- ${client}---, I am${this.name} - ${this.job} from ${this.city}`)
+  sayHi(arguments) {
+    console.log(`Hi, -- ${Array.from(arguments)}---, I am${this.name} - ${this.job} from ${this.city}`)
   }
 }
 
 
-export function delay(delay, callback, context,...args) {
-  setTimeout((callback.call(context, args)), delay)
+ export function delay(delay, callback, context,...arguments) {
+  setTimeout((callback.call(context, arguments)), delay)
 }
 
 delay(10000, user.sayHi, user, 'Tom', 'John', 'Bob',1, 2,3)
