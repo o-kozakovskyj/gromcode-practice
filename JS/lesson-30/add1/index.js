5ebc00fcf316c97766fb77660a5678fcb3610b1f
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-20 10:10:26 +0300

    fix

---
+++ JS/lesson-30/add1/index.js
@@ -0,0 +1,22 @@
+export const addImageV2 = (url) => {
+  const promise = new Promise(function (resolve, reject) {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'my photo');
+    imgElem.src = url;
+    document.querySelector('.page').append(imgElem);
+    resolve(imgElem);
+    reject(new Error('Image load is failed'));
+  });
+  return promise;
+};
+
+// examples
+
+addImageV2(
+  'https://p.b igstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'
+)
+  .then((data) => {
+    const { width, height } = data;
+    console.log({ width, height });
+  })
+  .catch((error) => console.log(error)); // ==> 'Image load failed'
--- JS/lesson-30/task1/index.js
+++ JS/lesson-30/task1/index.js
@@ -1,26 +1,32 @@
-
- export const addImage = imgSrc => {
-  const p = new Promise((resolve, reject)=> {
+export const addImage = (imgSrc) => {
+  const p = new Promise((resolve, reject) => {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'my photo');
     imgElem.src = imgSrc;
     const containerElem = document.querySelector('.page');
     containerElem.append(imgElem);
-   
+
     const onImageLoaded = () => {
       const { width, height } = imgElem;
       resolve({ width, height });
-    }
+    };
     imgElem.addEventListener('load', onImageLoaded);
-    imgElem.addEventListener('error', () => reject(new Error('Image load is failed')))
-    
+    imgElem.addEventListener('error', () =>
+      reject(new Error('Image load is failed'))
+    );
   });
   return p;
 };
 
- // examples
-const imgEl = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'
+// examples
+const imgEl =
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 const resultPromise = addImage(imgEl);
 
-resultPromise.catch(error => console.log(error))
-resultPromise.then(data =>document.querySelector('.image-size').textContent = `${data.width} x ${data.height}`);
+resultPromise.catch((error) => console.log(error));
+resultPromise.then(
+  (data) =>
+    (document.querySelector(
+      '.image-size'
+    ).textContent = `${data.width} x ${data.height}`)
+);
