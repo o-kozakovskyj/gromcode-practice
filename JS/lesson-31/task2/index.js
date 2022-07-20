47a33af99b2ad299d00dfdd61fdf19720755722c
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-20 19:44:56 +0300

    calc

---
+++ JS/lesson-31/task2/index.js
@@ -0,0 +1,25 @@
+export const asyncCalculator = (numbr) => {
+  const p = new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`Initial value:${numbr}`);
+      resolve(numbr);
+    }, 500);
+  })
+    .then(
+      (value) =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            const result = value * value;
+            console.log(`Squared value:${result}`);
+            resolve(result);
+          }, 500);
+        })
+    )
+    .then((value) => {
+      const result = value * 2;
+      console.log(`Doubled value:${result}`);
+      return result;
+    });
+};
+
+asyncCalculator(3).then((value) => console.log(value));
