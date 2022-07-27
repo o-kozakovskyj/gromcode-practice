8243ac0fa3326df253cb459af1a4a5af427afe8d
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-07-27 09:22:16 +0300

    test

--- JS/lesson-34/task1/src/scripts/tasksGateway.js
+++ JS/lesson-34/task1/src/scripts/tasksGateway.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://62deb5ec9c47ff309e7a60a0.mockapi.io/tasksList';
+const baseUrl = 'https://62deb5ec9c47ff309e7a60a0.mockapi.io/api/v1/tasksList';
 
 export const getTasksList = () => {
   return fetch(baseUrl).then((response) => response.json());
